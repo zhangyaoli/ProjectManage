f79fa6414ef1717c31b8a8c93cca771257b25789
zhangyaoli <714768559@qq.com>
2014-04-08 21:12:25 +0800

    Signed-off-by: zhangyaoli <714768559@qq.com>

--- cooperation.php
+++ cooperation.php
@@ -15,47 +15,142 @@ function countProject($username, $sql)
 {
     $count = 0;
     $projectSqlResult = mysql_query($sql);
-        while ($projectRows = mysql_fetch_array($projectSqlResult)) {
-            $projectStaffs = explode(',', $projectRows['pstaff']);
-            foreach ($projectStaffs as $projectStaff) {
-                if ($username == $projectStaff) {
-                    $count++;
-                }
+    while ($projectRows = mysql_fetch_array($projectSqlResult)) {
+        $projectStaffs = explode(',', $projectRows['pstaff']);
+        foreach ($projectStaffs as $projectStaff) {
+            if ($username == $projectStaff) {
+                $count++;
             }
         }
-        return $count;
+    }
+    return $count;
 }
 ?>
+<!DOCTYPE>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <style>
+    <title>项目管理系统</title>
+    <?php
+    include("navigation.php");
+    ?>
+    <style type="text/css">
+        <!--
+        body {
+            margin-left: 0px;
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 0px;
+        }
+
+        .tabfont01 {
+            font-family: "宋体";
+            font-size: 9px;
+            color: #555555;
+            text-decoration: none;
+            text-align: center;
+        }
+
+        .font051 {
+            font-family: "宋体";
+            font-size: 12px;
+            color: #333333;
+            text-decoration: none;
+            line-height: 20px;
+        }
+
+        .font201 {
+            font-family: "宋体";
+            font-size: 12px;
+            color: #FF0000;
+            text-decoration: none;
+        }
+
+        .button {
+            font-family: "宋体";
+            font-size: 14px;
+            height: 37px;
+        }
+
+        html {
+            overflow-x: auto;
+            overflow-y: auto;
+            border: 0;
+        }
+        table.tab_data {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .tab_data th {
+            background: skyblue;
+            font-size: 12px;
+            font-weight: 100;
+            height: 28px;
+            text-align: left;
+            width:20%;
+        }
+
+        .tab_data tr td {
+            padding: 7px;
+            background: #fff;
+            color: #333;
+            text-align: left;
+        }
+        .tab_data tr td div{
+            padding: 3px;
+        }
+        -->
+        .user_list td div{
+            font-size:20px ;
+            text-align: center;
+        }
+        .user_head td{
+            text-align: center;
+            width: 200px;
+            font-size: 22px;
+
+        }
+        .user_table{
+            margin-left: 20px;
+            border: 1px solid #000000;
+            border-collapse: collapse;
+        }
+        .user_table tr td {
+            border: 1px solid #888888;
+        }
     </style>
+    <link href="css/css.css" rel="stylesheet" type="text/css"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="../js/xiangmu.js"></script>
 </head>
+</html>
+
+
+
 <body>
-<table>
-    <tr>
+<table class="user_table">
+    <tr class="user_head">
         <td>人员</td>
         <td>项目完成数</td>
         <td>进行中的项目</td>
     </tr>
     <?php
-
     while ($userRows = mysql_fetch_array($userSqlResult)){
         ?>
-        <tr>
-          <td><?php echo explode('@', $userRows['username'])[0]; ?></td>
-            <td>
+        <tr class="user_list">
+          <td><div><?php echo explode('@', $userRows['username'])[0]; ?></div></td>
+            <td><div>
                 <?php
                 $projectSql = "select * from project where pspeed=100";
                 echo countProject($userRows['username'], $projectSql);
-                ?>
+                ?></div>
             </td>
-            <td>
+            <td><div>
                 <?php
                 $projectSql = "select * from project where pspeed!=100";
                 echo countProject($userRows['username'], $projectSql);
-                ?>
+                ?></div>
             </td>
         </tr>
     <?php } ?>
